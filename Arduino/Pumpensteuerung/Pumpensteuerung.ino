cdf922c9da5e6b48b0dcac424d304134f489e5d2
ACADON\jks <jks@acadon.de>
2018-11-04 13:26:32 +0100

    Added ErrorHandling to Pumpensteuerung

--- Arduino/Pumpensteuerung/Pumpensteuerung.ino
+++ Arduino/Pumpensteuerung/Pumpensteuerung.ino
@@ -2,14 +2,15 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
+//Wifi Communication
 const char* ssid = "Coredy E300";
 const char* password = "<Insert Password here>";
+const char* hashValue= "<Insert HASH here>";
 const char* host = "192.168.10.118";
-String url = "/SensorValue/InsertValue";
 const int httpPort = 50000;
 
+//Sensor Values
 int sensorID = 1;
-
 int inputHumidityPort = 2;
 int outputPump = 5;
 int outputLED = 0;
@@ -17,7 +18,35 @@ int outputLED = 0;
 
 
 
+void postErrorMessage(int sensor, int errorType, String message){
+  String url = "/Error/InsertError";
+  WiFiClient client = connectWifi();
+  String strValue("sensorId=");
+  strValue += String(sensor);
+  strValue += String("&errorType=");
+  strValue += String(errorType);
+  strValue += String("&message=");
+  strValue += String(message);
+  strValue += getHashString();
+  
+  
+  #ifdef debug
+    Serial.println(strValue);
+    Serial.println(strValue.length());
+  #endif
+  
+  client.print(String("POST ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Content-Type: application/x-www-form-urlencoded\r\n" +               
+               "Content-Length: " + strValue.length() + "\r\n" +
+               "\r\n" +
+               strValue
+               );
+}
+
+
 void postSensorValue(int sensorType, int sensor, int value){
+  String url = "/SensorValue/InsertValue";
   WiFiClient client = connectWifi();
   String strValue("sensorType=");
   strValue += String(sensorType);
@@ -25,6 +54,7 @@ void postSensorValue(int sensorType, int sensor, int value){
   strValue += String(sensor);
   strValue += String("&value=");
   strValue += String(value);
+  strValue += getHashString();
   
   #ifdef debug
     Serial.println(strValue);
@@ -57,20 +87,26 @@ void loop() {
   if(Humidity == 1){
     bewaessern();
   }
-
   // print out the state of the button:
 
   delay(1000);        // delay in between reads for stability
 }
 
 void bewaessern(){
-  digitalWrite(outputLED, HIGH);
-  digitalWrite(outputPump, HIGH);
-  delay(10000);
   digitalWrite(outputLED, LOW);
+  digitalWrite(outputPump, HIGH);
+  delay(11000);
+  digitalWrite(outputLED, HIGH);
   digitalWrite(outputPump, LOW);
   postSensorValue(0,sensorID,HIGH);
-  delay(5000); 
+  delay(15000);
+  if(digitalRead(inputHumidityPort)){
+    String errorMessage = String("Wasserstand niedrig. Bitte Wasser in Sensor " + String(sensorID) + " nachfüllen.");
+    postErrorMessage(sensorID, 2, errorMessage);
+    while(true){
+      delay(1000);
+    }
+  }
 }
 
 WiFiClient connectWifi(){
@@ -106,4 +142,8 @@ WiFiClient connectWifi(){
     #endif
     return client;
   }
+}
+
+String getHashString(){
+    return String("&hash=" + hashValue);
 }
